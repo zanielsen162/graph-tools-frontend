e24999cde7282bae24038d1fd5f6bd4afbc7a4fb
zanielsen <zanielsen@ucsb.edu>
2025-07-29 11:56:38 -0700

    form row component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ import InputTextbox from "@/components/atoms/InputTextbox";
 import Dropdown from "@/components/atoms/Dropdown";
 import Checkbox from "@/components/atoms/Checkbox";
 import NavBar from "@/components/molecules/NavBar";
+import FormRow from "@/components/molecules/FormRow";
+import structures from "@/data/structures.json";
 
 export default function Home() {
   return (
     
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center  min-h-screen pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
       </main>
     </div>
   );
