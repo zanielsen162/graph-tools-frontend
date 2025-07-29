d0cdd6794af46fe75a36764968898c942dddf53e
zanielsen <zanielsen@ucsb.edu>
2025-07-29 15:22:02 -0700

    health endpoint and container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import Form from "@/components/organisms/Form";
 
 export default function Home() {
   return (
-    
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center  min-h-screen pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       </main>
