da77ca7b1e14ce97400e1442a24ed69fcde9084b
zanielsen <zanielsen@ucsb.edu>
2025-07-29 10:37:40 -0700

    navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Button from "@/components/atoms/Button";
 import InputTextbox from "@/components/atoms/InputTextbox";
 import Dropdown from "@/components/atoms/Dropdown";
 import Checkbox from "@/components/atoms/Checkbox";
+import NavBar from "@/components/molecules/NavBar";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center  min-h-screen pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-
+        
       </main>
     </div>
   );
